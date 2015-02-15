fac2aac140582de58be61d7038ec476ab2b3f196
Jeff Yoshimi <jeffyoshimi@yahoo.com>
2015-02-15 09:36:11 -0800

    Fixing a few problems with the way I had the javascript stuff set up.

--- html_fun_2/jeff.js
+++ html_fun_2/jeff.js
@@ -1,3 +1,7 @@
+// An array
+var myArray = ["Butovens", "Gina", "Tim", "Umesh", "Yuliya"];
+
+// Main function
 var main = function() {
     alert("Hello!");
     console.log("Hello console");
@@ -9,42 +13,12 @@ var main = function() {
     $("body").append(num + "<br>");
     $("body").append(str + "<br>");
 
-    // An array
-    var myArray = ["Butovens", "Gina", "Tim", "Umesh", "Yuliya"];
     $("body").append(myArray[3] + "<br>");
     // $("body").append(myArray + "<br>");
 
     // A for loop
     for (i = 0; i < myArray.length; i++) {
         $("body").append(myArray[i] + "<br>");
-    }
-
-    // A function
-    function addBling(toBeBlinged) {
-        $("body").append("~~~~~$$$$$ " + toBeBlinged + " $$$$$~~~~~<br>");
-    }
-    for (i = 0; i < myArray.length; i++) {
-        addBling(myArray[i]);
-    }
-
-    // Show Google function
-    function showGoogle() {
-        window.open("http://www.google.com");
-    }
-
-    // Write some text to text field
-    function sayPups() {
-        document.getElementById("catspups").value = "Puppies";
-    }
-
-    // Test if else
-    function ifElseTest() {
-        if (document.getElementById("catspups").value === "password") {
-            alert("Welcome to the magic kingdom...");
-            window.open("https://secure.parksandresorts.wdpromedia.com/resize/mwImage/1/900/360/90/wdpromedia.disney.go.com/media/wdpro-assets/parks-and-tickets/destinations/magic-kingdom/magic-kingdom-00-full.jpg?12032013080923");
-        } else {
-            alert("Fail!");
-        }
     }
 
     // Messing around with JQuery
@@ -62,4 +36,33 @@ var main = function() {
 
 };
 
+// A function
+function addBling(toBeBlinged) {
+    $("body").append("~~~~~$$$$$ " + toBeBlinged + " $$$$$~~~~~<br>");
+}
+for (i = 0; i < myArray.length; i++) {
+    addBling(myArray[i]);
+}
+
+// Show Google page
+function showGoogle() {
+    window.open("http://www.google.com");
+}
+
+// Write some text to text field
+function sayPups() {
+    document.getElementById("catspups").value = "Puppies";
+}
+
+// Test if else
+function ifElseTest() {
+    if (document.getElementById("catspups").value === "password") {
+        alert("Welcome to the magic kingdom...");
+        window.open("https://secure.parksandresorts.wdpromedia.com/resize/mwImage/1/900/360/90/wdpromedia.disney.go.com/media/wdpro-assets/parks-and-tickets/destinations/magic-kingdom/magic-kingdom-00-full.jpg?12032013080923");
+    } else {
+        alert("Fail!");
+    }
+}
+
+// Don't do jquery stuff until page has loaded
 $(document).ready(main);
